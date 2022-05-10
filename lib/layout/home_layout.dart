e3124513f61f6b0b33cb89df066f720e46d43fc8
kamar2000 <mr.kamer2000@gmail.com>
2022-05-10 17:02:26 +0200

    Localization

--- lib/layout/home_layout.dart
+++ lib/layout/home_layout.dart
@@ -222,7 +222,34 @@ class HomeLayout extends StatelessWidget {
               child: SingleChildScrollView(
                 child: Column(
                   children: [
-                    MyHeaderDrawer(),
+                    Container(
+                      color:Theme.of(context).backgroundColor,
+                      width: double.infinity,
+                      height: 200.0,
+                      child: Padding(
+                        padding:  const EdgeInsets.only(top: 16.0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children:  [
+                            Container(
+
+                              margin:const EdgeInsets.only(bottom: 10.0),
+                              height: 70.0,
+                              decoration:const BoxDecoration(
+                                shape: BoxShape.circle,
+                                image:DecorationImage(
+                                  image: NetworkImage('https://variety.com/wp-content/uploads/2020/12/Adam_Driver.png'),
+                                ),
+                              ),
+                            ),
+                            const Text('Abdulrahman Kamar',style:TextStyle(color: Colors.white,fontSize: 20,) ,),
+                            Text('mr.kamer2000@gmail.com',style:TextStyle(color: Colors.grey[200],fontSize: 14,) ,),
+
+
+                          ],
+                        ),
+                      ),
+                    ),
                     MyDrawerList(),
                   ],
                 ),
