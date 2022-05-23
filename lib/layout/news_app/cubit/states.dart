d9348212da2a0d41764ead609f8f3ab4b49f626e
kamar2000 <mr.kamer2000@gmail.com>
2022-05-24 00:41:44 +0200

    News App

---
+++ lib/layout/news_app/cubit/states.dart
@@ -0,0 +1,7 @@
+abstract class NewsStates{}
+
+class NewsInitialState extends NewsStates{}
+
+class NewsBottomNavState extends NewsStates{}
+
+class NewsChangeIndexBottomNavBarState extends NewsStates{}
